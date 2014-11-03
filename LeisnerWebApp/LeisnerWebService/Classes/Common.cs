acba9795e56fe1648bd4fef64d6ce99f2ac9a16f
SebastianBiris <sebgarcea@yahoo.co.uk>
2014-11-03 10:02:07 +0100

    sd
    sddsddd

---
+++ LeisnerWebApp/LeisnerWebService/Classes/Common.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeisnerWebService.Classes
+{
+    public class Common
+    {
+        int personId, weekId, dayId, hourId, sizeId;
+        public Common(int personId)
+        {
+            this.personId = personId;
+            
+        }
+        public int SizeId
+        {
+            get { return sizeId; }
+            set { sizeId = value; }
+        }
+
+        public int HourId
+        {
+            get { return hourId; }
+            set { hourId = value; }
+        }
+
+        public int DayId
+        {
+            get { return dayId; }
+            set { dayId = value; }
+        }
+
+        public int WeekId
+        {
+            get { return weekId; }
+            set { weekId = value; }
+        }
+
+        public int PersonId
+        {
+            get { return personId; }
+            set { personId = value; }
+        }
+    }
+}
--- LeisnerWebApp/LeisnerWebService/IDBAccessService.cs
+++ LeisnerWebApp/LeisnerWebService/IDBAccessService.cs
@@ -6,6 +6,8 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 using LeisnerWebApp.App_Code;
+using LeisnerWebService.Classes;
+
 namespace LeisnerWebService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
@@ -14,18 +16,28 @@ namespace LeisnerWebService
     {
 
         [OperationContract]
-        void  FindPerson();
+        void FindPerson();
 
         [OperationContract]
         List<Person> FindAllPersons();
+        
         [OperationContract]
         void FindTime();
+        
         [OperationContract]
         List<Time> FindAllTimes();
+        
         [OperationContract]
         void SavePerson(int Status, string Email, string Password, string Name, string Address, string ChildsName, string Doctor, string DateOfBirth);
+        
         [OperationContract]
         void SaveHour(string Hour);
+
+        [OperationContract]
+        List<Common> FindInfo(int PersonId);
+
+        [OperationContract]
+        void SaveInfo(Person personId, Week weekId, Time hourId, Day dayId, PeePee peePeeId);
     }
 }
 
